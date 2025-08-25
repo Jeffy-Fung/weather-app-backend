6ee9256579241ead0fe6b14551ac47312afe547f
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-08-25 15:39:40 +0800

    feat: serialise current weather api data

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -24,13 +24,8 @@ const getCurrentWeather = async (req, res) => {
     const result = await weatherService.getCurrentWeather(lang);
     
     if (result.success) {
-      return res.status(200).json({
-        data: result.data,
-        cached: result.cached || false,
-        timestamp: result.timestamp,
-        source: result.source,
-        language: result.language
-      });
+      response = serialize_result_data(result)
+      return res.status(200).json(response);
     } else {
       return res.status(500).json({
         error: 'Failed to fetch weather data',
@@ -46,6 +41,50 @@ const getCurrentWeather = async (req, res) => {
     });
   }
 };
+
+const serialize_result_data = (result) => {
+  const { data, cached, timestamp, source, language } = result;
+
+  const rainfallData = data.rainfall.data;
+  const functionalRainfall = rainfallData.filter(item => item.main == "FALSE");
+  const averageRainfall = functionalRainfall.reduce((acc, item) => acc + item.max, 0) / functionalRainfall.length;
+
+  const allUsedUnit = rainfallData.map(item => item.unit);
+  const uniqueUnits = [...new Set(allUsedUnit)];
+  if (uniqueUnits.length > 1) throw new Error('Rainfall data has multiple units');
+
+  return {
+    data: {
+      averageRainfall: {
+        unit: uniqueUnits[0],
+        value: averageRainfall,
+        startTime: data.rainfall.startTime,
+        endTime: data.rainfall.endTime
+      },
+      temperature: {
+        // TODO: better retrieve result from data collected from specific location
+        ...data.temperature.data[1],
+        recordTime: data.temperature.recordTime,
+      },
+      specialWxTips: data.specialWxTips,
+      warningMessage: data.warningMessage,
+      uvindex: {
+        ...data.uvindex.data[0],
+        recordDesc: data.uvindex.recordDesc,
+      },
+      humidity: {
+        ...data.humidity.data[0],
+        recordTime: data.humidity.recordTime,
+      },
+      updateTime: data.updateTime,
+      tcmessage: data.tcmessage,
+    },
+    cached: cached || false,
+    timestamp: timestamp,
+    source: source,
+    language: language
+  }
+}
 
 module.exports = {
   getCurrentWeather
